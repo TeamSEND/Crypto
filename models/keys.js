66c6e768001042d6883316ef5fd60f48ca2fd0e0
Daryl Cimafranca <cimafrancad45@gmail.com>
2018-08-18 10:25:37 -0400

    removed keys

--- models/keys.js
+++ models/keys.js
@@ -1,6 +1,1 @@
-const newsKey = "f5090fe009de4358b098c43bb4137e89";
-const cmcKey = "3c4f7072-2810-498f-8b55-619f95408199";
-
-
-module.exports = newsKey
-module.exports = cmcKey
+"there are no keys"
