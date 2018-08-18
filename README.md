9743b227c57c14606aa3240a43b13bf8974ecd0d
dotRollen <edwardnnz@gmail.com>
2018-08-18 17:35:41 -0400

    Passport integrated succesfully, and user model created, signup and signin functional

--- README.md
+++ README.md
@@ -2,18 +2,35 @@
 
 Crypto is a crpyto currency market and news digest app. This allows a user to view crypto currency news and search market cap in one place. With the feature to save your results to your personal account for quick views.
 
-## Module Dependencies
+## How to setup
+
+### Development
+
+`npm run dev:server`
+
+### Production
+
+## Technologies Used
+
+### Module Dependencies
 * Node.js v10.8.0 [Documentation](https://nodejs.org/en/docs/)
 * Express.js v4.16.3 [Documentation](https://www.npmjs.com/package/express)
 * Body-parser v1.18.3 [Documentation]()
-* dotENv v6.0.0 [Documentation]()
-* Express-handlebars v3.0.0 [Documentation]()
-* Mysql2 v1.5.3 [Documentation]()
-* Sequelize v4.38.0 [Documentation]()
+* dotEnv v6.0.0 [Documentation](https://www.npmjs.com/package/dotenv)
+* Express-handlebars v3.0.0 [Documentation](https://www.npmjs.com/package/express-handlebars)
+* Mysql2 v1.5.3 [Documentation](https://www.npmjs.com/package/mysql2)
+* Sequelize v4.38.0 [Documentation](https://www.npmjs.com/package/sequelize)
 
-## Experimental Dependencies
+### Experimental Dependencies
 * Charts.js [Documentation](https://www.npmjs.com/package/charts)
+* uuid [Documentation](https://www.npmjs.com/package/uuid)
+* express-session [Documentation](https://www.npmjs.com/package/express-session)
+* session-file-store [Documentation](https://www.npmjs.com/package/session-file-store)
+* passport [Documentation](https://www.npmjs.com/package/passport)
+* passport-local [Documentation](https://www.npmjs.com/package/passport-local)
+* bCrypt [Documentation](https://www.npmjs.com/package/bcrypt)
 
-## RESTful APIs
+
+### RESTful APIs
 * CoinMarketCap [Documentation](https://coinmarketcap.com/)
 * CryptoCoinNews [Documentation](https://newsapi.org/s/crypto-coins-news-api)
--- server.js
+++ server.js
@@ -1,19 +1,45 @@
+// Imports for app dependencies
 require("dotenv").config();
 var express = require("express");
+var passport = require("passport");
+var session = require("express-session");
 var bodyParser = require("body-parser");
 var exphbs = require("express-handlebars");
-
+var FileStore = require("session-file-store")(session);
+var uuid = require("uuid");
 var db = require("./models");
 
+// Define express http server object and port
 var app = express();
 var PORT = process.env.PORT || 3000;
 
-// Middleware
+// BodyParser for JSON data handling by express
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+// Allow express to create sessions when a client connects
+app.use(
+  session({
+    genid: function(req) {
+      return uuid(); // use UUIDs for session IDs
+    },
+    store: new FileStore(),
+    secret: process.env.SECRET_KEY || "this-is-super-secret",
+    resave: false,
+    saveUninitialized: true
+  })
+);
+
+// Initialize passport and passport session monitoring in express
+app.use(passport.initialize());
+app.use(passport.session());
+//load passport strategies
+require("./config/passport.js")(passport, db.user);
+
+// Static files server to clients
 app.use(express.static("public"));
 
-// Handlebars
+// Handlebars setup with express
 app.engine(
   "handlebars",
   exphbs({
@@ -22,14 +48,14 @@ app.engine(
 );
 app.set("view engine", "handlebars");
 
-// Routes
+// Routes for client requests
 require("./routes/apiRoutes")(app);
+require("./routes/authRoutes")(app, passport);
 require("./routes/htmlRoutes")(app);
-
-var syncOptions = { force: false };
 
 // If running a test, set syncOptions.force to true
 // clearing the `testdb`
+var syncOptions = { force: false };
 if (process.env.NODE_ENV === "test") {
   syncOptions.force = true;
 }
