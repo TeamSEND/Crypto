7ffda6d70411c77a1ca75e6c9a3724ef43564be6
Paradox <cdesai516@gmail.com>
2018-08-25 17:04:32 -0400

    draftBLockCHain101V1

--- models/coin.js
+++ models/coin.js
@@ -1,38 +1,42 @@
 module.exports = function(sequelize, DataTypes) {
-  var Coin = sequelize.define("coin", {
-    id: {
-      autoIncrement: true,
-      primaryKey: true,
-      type: DataTypes.INTEGER
-    },
+    var Coin = sequelize.define("coin", {
+      id: {
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER
+      },
+      coinid: {
+        type: DataTypes.INTEGER,
+     
+      },
+      name: {
+        type: DataTypes.STRING,
+     
+      },
+  
+      symbol: {
+        type: DataTypes.STRING,
+      
+      },
+  
+      category: {
+        type: DataTypes.TEXT
+        },
+  
+      slug: {
+        type: DataTypes.STRING,
+       },
+      
 
-    coinid: {
-      type: DataTypes.INTEGER
-    },
+     logo : {
+        type: DataTypes.STRING
 
-    name: {
-      type: DataTypes.STRING
+      },
+  
+      tags: {
+        type: DataTypes.STRING
     },
-
-    symbol: {
-      type: DataTypes.STRING
-    },
-
-    category: {
-      type: DataTypes.TEXT
-    },
-
-    slug: {
-      type: DataTypes.STRING
-    },
-
-    logo: {
-      type: DataTypes.STRING
-    },
-
-    tags: {
-      type: DataTypes.STRING
-    }
-  });
-  return Coin;
-};
+}),
+    return Coin;
+  };
+  
