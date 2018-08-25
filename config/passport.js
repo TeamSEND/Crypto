2938158e6e59bf444bc2849e8f03cc5c7a11de58
dotRollen <edwardnnz@gmail.com>
2018-08-25 11:34:18 -0400

    Added user profile page and user change settings page

--- config/passport.js
+++ config/passport.js
@@ -33,8 +33,9 @@ module.exports = function(passport, user) {
             var data = {
               email: email,
               password: userPassword,
-              firstname: req.body.firstname,
-              lastname: req.body.lastname
+              displayName: req.body.displayname,
+              firstName: req.body.firstname,
+              lastName: req.body.lastname
             };
             User.create(data).then(function(newUser, created) {
               if (!newUser) {
@@ -93,6 +94,54 @@ module.exports = function(passport, user) {
               message: "Something went wrong with your Signin"
             });
           });
+      }
+    )
+  );
+
+  passport.use(
+    "change-settings",
+    new LocalStrategy(
+      {
+        usernameField: "email",
+        passwordField: "password",
+        passReqToCallback: true
+      },
+
+      function(req, email, password, done) {
+        var generateHash = function (password) {
+          return bCrypt.hashSync(password, saltRounds);
+        };
+
+        User.findOne({
+          where: {
+            email: email
+          }
+        }).then(function(user) {
+          console.log(user);
+          // if (user) {
+          //   return done(null, false, {
+          //     message: "That email is already take"
+          //   });
+          // } else {
+          //   var userPassword = generateHash(password);
+          //   var data = {
+          //     email: email,
+          //     password: userPassword,
+          //     displayName: req.body.displayname,
+          //     firstName: req.body.firstname,
+          //     lastName: req.body.lastname
+          //   };
+          //   User.create(data).then(function(newUser, created) {
+          //     if (!newUser) {
+          //       return done(null, false);
+          //     }
+
+          //     if (newUser) {
+          //       return done(null, newUser);
+          //     }
+          //   });
+          // }
+        });
       }
     )
   );
--- modules/coinmarketcap.js
+++ modules/coinmarketcap.js
@@ -1,40 +1,41 @@
 //dependencies
-const secretKey = require("../config/key.js");
-const keyz = secretKey.CMC;
+var secretKey = require("../config/key.js");
+var keyz = secretKey.CMC;
 // const key = "3c4f7072-2810-498f-8b55-619f95408199";
-console.log(keyz)
+console.log(keyz);
 
-const axios = require('axios');
-var coinData = ""
+var axios = require("axios");
+var coinData = "";
 
-
-var CMCquery = function (APIkey) {
+var CMCquery = function(APIkey) {
   //searches for the crypto currency type and ID
   this.key = APIkey;
-  this.url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency'
+  this.url = "https://pro-api.coinmarketcap.com/v1/cryptocurrency";
   this.config = {
     headers: {
-      "X-CMC_PRO_API_KEY": this.key,
+      "X-CMC_PRO_API_KEY": this.key
     }
-  }
-//using axios to get the api call
-  this.axiosRequest = function (request) {
-    axios.get(request, this.config)
-      .then(function (response) {
+  };
+
+  // Using axios to get the api call
+  this.axiosRequest = function(request) {
+    axios
+      .get(request, this.config)
+      .then(function(response) {
         //parses the response as a JSON object
         var coinJSON = Object.values(JSON.parse(JSON.stringify(response.data)));
         //sets the coinData variable with the parsed object
         coinData = coinJSON;
-        console.log(coinData)
+        console.log(coinData);
         return coinData;
       })
       //console logs an error.
-      .catch(function (error) {
-        console.log(error.response.status)
+      .catch(function(error) {
+        console.log(error.response.status);
       });
   };
   //gets the top ten ranked cryptocurrencies
-  this.bestCryptos = function () {
+  this.bestCryptos = function() {
     var topTen = this.url + "/listings/latest?start=1&limit=10";
     this.axiosRequest(topTen);
   };
@@ -46,7 +47,6 @@ var CMCquery = function (APIkey) {
 // };
 
 var test = new CMCquery(keyz);
-
 test.bestCryptos();
 
 // module.exports = function(sequelize, data){
