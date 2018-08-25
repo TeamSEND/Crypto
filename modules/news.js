bcd3208aac0b8899654d519221a0b306f091141d
Daryl Cimafranca <cimafrancad45@gmail.com>
2018-08-25 16:52:04 -0400

    made changes to modules

--- modules/news.js
+++ modules/news.js
@@ -1,27 +1,27 @@
 //dependencies
 
 const NewsAPI = require("newsapi");
-const secretKey = require("../config/key.js");
-const key = secretKey.NEWS;
-const newsapi = new NewsAPI(key);
+const keys = require("../config/key");
 //placeholder for search
-var search = "Bitcoin";
 
 //generic search for trending articles. Only using crypto-coins-news
-newsapi.v2.topHeadlines({
-    sources: "crypto-coins-news",
-    q: search
-}).then(response => {console.log(response)});
+var newsObject = function (APIkey) {
+    this.newsapi = new NewsAPI(APIkey);
+    this.articles = {};
+    this.topArticles = async function (search) {
+        await this.newsapi.v2.topHeadlines({
+            sources: "crypto-coins-news",
+            q: search
+        }).then(
+            response => 
+            {
+                this.articles = response;
+            });
+            return this.articles
+    };
+}
 
-//placeholder for printing on to handlebar page
+var news =  new newsObject(keys.NEWS)
+var topTen = news.topArticles("Bitcoin");
 
-//--------------------------------------
-
-
-
-
-//--------------------------------------
-
-// module.exports = function(sequelize, data){
-    
-// }
+console.log(topTen)
