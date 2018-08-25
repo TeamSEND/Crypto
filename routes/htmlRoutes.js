f31dbbcee0c1a886e234ee6730db51298d534388
Daryl Cimafranca <cimafrancad45@gmail.com>
2018-08-25 14:14:10 -0400

    blah

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,28 +1,23 @@
 var db = require("../models");
-var cmc = require("../modules/coinmarketcap.js")
-var news = require("../modules/news.js")
+var cmc = require("../modules/coinmarketcap.js");
+var news = require("../modules/news.js");
+const keys = require("../config/key");
 
 module.exports = function(app) {
   // Load index page
   app.get("/", function(req, res) {
+<<<<<<< HEAD
     res.render("index", { user: req.user });
+=======
+      var topTen = cmc.bestCryptos(keys.CMC);
+      console.log(topTen)
+      res.render("index", {
+        topTen: topTen,
+      })
+      
+>>>>>>> blah
   });
 
-  // Load example page and pass in an example by id
-  // app.get("/example/:id", function(req, res) {
-  //   db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
-  //     res.render("example", {
-  //       example: dbExample
-  //     });
-  //   });
-  // });
-
-  app.get("/topten", function (req, res){
-    cmc.test.bestCryptos();
-    res.render("", {
-
-    });
-  });
   // Render 404 page for any unmatched routes
   app.get("*", function(req, res) {
     res.render("404", { user: req.user });
