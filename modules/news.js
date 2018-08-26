ad558fd18573d189e3519cae7eb37be334e86748
Daryl Cimafranca <cimafrancad45@gmail.com>
2018-08-26 01:22:25 -0400

    trying out new code on news.js

--- modules/news.js
+++ modules/news.js
@@ -9,19 +9,28 @@ var newsObject = function (APIkey) {
     this.newsapi = new NewsAPI(APIkey);
     this.articles = {};
     this.topArticles = async function (search) {
-        await this.newsapi.v2.topHeadlines({
-            sources: "crypto-coins-news",
-            q: search
-        }).then(
-            response => 
-            {
-                this.articles = response;
-            });
-            return this.articles
-    };
+        return new Promise((resolve, reject) => {
+
+            const error = false;
+          
+
+            this.newsapi.v2.topHeadlines({
+                sources: "crypto-coins-news",
+                q: search
+            }).then(
+                response => {
+                    this.articles = response;
+                    return this.articles;
+                });  
+                if (!error){
+                resolve()
+            } else {
+                reject("Error: unable to handle request.")
+            }
+        });
+    }
 }
 
-var news =  new newsObject(keys.NEWS)
-var topTen = news.topArticles("Bitcoin");
+// var news = new newsObject(keys.NEWS)
+// var topTen = news.topArticles("Bitcoin");
 
-console.log(topTen)
