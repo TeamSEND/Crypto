5445b00340758769d8471d382349174592efd602
dotRollen <edwardnnz@gmail.com>
2018-08-26 18:00:50 -0400

    Pushing latest updates

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,48 @@
+// General purpose functionality
+
+// Search for a keyword
+var Search = (function() {
+  // POST request for search input
+  var searchPOST = function(keyword) {
+    return $.ajax({
+      headers: {
+        "Content-Type": "application/json"
+      },
+      type: "POST",
+      url: "/search",
+      data: JSON.stringify(keyword),
+      success: function(response) {
+        console.log(response);
+        if (response.result === "redirect") {
+          window.location.replace(response.url);
+        }
+      }
+    });
+  };
+
+  return {
+    init: function() {
+      document
+        .querySelector("#search")
+        .addEventListener("keypress", function(event) {
+          var key = event.which || event.keyCode;
+          if (key === 13) {
+            var keyword = {
+              keyword: document.querySelector("#search").value
+            };
+            if (keyword) {
+              searchPOST(keyword);
+            }
+          }
+        });
+    }
+  };
+})();
+
 window.onload = function() {
   // Initializes all the class objects to setup for the proper functionality
   // of the full app features
   // var xreq = XHTMLRequests.init();
+  Search.init();
   Session.init();
 };
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -11,11 +11,35 @@ module.exports = function(app) {
   // Renders front page with top articles
   app.get("/", function(req, res) {
     cnAPI.topHeadlines().then(function(articles) {
-      console.log(articles);
       res.render("index", {
         user: req.user,
         articles: articles
       });
+    });
+  });
+
+  app.get("/search", function(req, res) {
+    cmcAPI.bestCryptos().then(function(cryptocoins) {
+      console.log(cryptocoins);
+      res.render("search", {
+        user: req.user,
+        cryptocoins: cryptocoins
+      });
+    });
+  });
+
+  app.post("/search", function(req, res) {
+    console.log("got it!");
+    var keyword = req.body.keyword;
+    res
+      .status(200)
+      .send({ result: "redirect", url: "/search/results=" + keyword });
+  });
+
+  app.get("/search/results=:keyword", function(req, res) {
+    res.render("searchresults", {
+      user: req.user,
+      keyword: req.params.keyword
     });
   });
 
