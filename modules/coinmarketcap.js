91da8a75158a7b7927222910c7921dd38d0176f1
dotRollen <edwardnnz@gmail.com>
2018-08-26 15:37:39 -0400

    Added Daryl's API modules, htmlRoutes.js renders article data to the handlebars

--- modules/coinmarketcap.js
+++ modules/coinmarketcap.js
@@ -1,54 +1,41 @@
-//dependencies
-var secretKey = require("../config/key.js");
-var keyz = secretKey.CMC;
-// const key = "3c4f7072-2810-498f-8b55-619f95408199";
-console.log(keyz);
-
 var axios = require("axios");
-var coinData = "";
 
-var CMCquery = function(APIkey) {
+module.exports = function(APIkey) {
   //searches for the crypto currency type and ID
-  this.key = APIkey;
-  this.url = "https://pro-api.coinmarketcap.com/v1/cryptocurrency";
-  this.config = {
+  this.coinData;
+  var key = APIkey;
+  var url =
+    "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?start=1&limit=10";
+  var config = {
     headers: {
-      "X-CMC_PRO_API_KEY": this.key
+      "X-CMC_PRO_API_KEY": key
     }
   };
 
-  // Using axios to get the api call
-  this.axiosRequest = function(request) {
-    axios
-      .get(request, this.config)
-      .then(function(response) {
-        //parses the response as a JSON object
-        var coinJSON = Object.values(JSON.parse(JSON.stringify(response.data)));
-        //sets the coinData variable with the parsed object
-        coinData = coinJSON;
-        console.log(coinData);
-        return coinData;
-      })
-      //console logs an error.
-      .catch(function(error) {
-        console.log(error.response.status);
-      });
-  };
   //gets the top ten ranked cryptocurrencies
   this.bestCryptos = function() {
-    var topTen = this.url + "/listings/latest?start=1&limit=10";
-    this.axiosRequest(topTen);
+    return (
+      axios
+        .get(url, config)
+        .then(function(response) {
+          // parses the response as a JSON object
+          var coinJSON = Object.values(
+            JSON.parse(JSON.stringify(response.data))
+          );
+          // sets the coinData variable with the parsed object
+          this.coinData = coinJSON;
+          return this.coinData;
+        })
+        // console logs an error.
+        .catch(function (error) {
+          console.log(error);
+        })
+    );
   };
 };
 
-//----- placeholder for cryptocurrency symbol search
-// this.info = function () {
-//   console.log("this is information")
-// };
+// Testing & Debugging
+// var secretKey = require("../config/key.js");
 
-var test = new CMCquery(keyz);
-test.bestCryptos();
-
-// module.exports = function(sequelize, data){
-
-// }
+// var test = new CMCquery(secretKey.CMC);
+// test.bestCryptos().then(function (topTen) { console.log(topTen) });
